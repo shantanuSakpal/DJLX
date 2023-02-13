4a16dae2948c4b82aac5c9350cb5575f46148c6d
abhinike <abhishekssinha2003@gmail.com>
2023-02-13 15:34:40 +0530

    login page added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React , {useState}from 'react'
+import auth from '../firebaseConfig';
 
 const Login = () => {
+    const [email, setemail] = useState('');
+    const [password, setpassword] = useState('');
+
+    const signin = (e) => {
+        //todo
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) =>{
+            
+            console.log("success")
+        }
+        ).catch((error) => {
+            console.log('{error}')
+
+        })
+
+    }
+
+
   return (
     <div>
-        <h1 className='text-lg'>Login IN</h1>
-        <input type= {'email'} placeholder = 'Enter your name'> email </input>
-        <input type = { 'password'} placeholder = 'Enter your password'> password </input>
-      
+        <h1 className='text-lg'>Log IN</h1>
+        <form onSubmit={signin}>
+
+            <div className='p-2 '>
+                
+                <label htmlFor="email">email</label>
+                <input type= {'email'} placeholder = 'Enter your name' value={email} onChange = {(e) => setemail(e.target.value)}/>
+
+
+                
+
+            </div >
+
+            <div className='p-2 block'>
+                 <label htmlFor="password">password</label>
+
+                <input type = { 'password'} placeholder = 'Enter your password' value={password} onChange = {(e) => setpassword(e.target.value)}/>
+
+                <button type='submit'>Log IN</button>
+            </div>
+
+        
+        </form>
     </div>
   )
 }
